dc452dbaa533fd5541e45343aa7fc03918093b4d
shadowshan125 <ajaykrishnan114cs@gmail.com>
2021-09-18 14:38:57 +0530

    Second Update

---
+++ studentservice/src/main/java/studentRepository/StudentRepository1.java
@@ -0,0 +1,10 @@
+package studentRepository;
+
+import org.springframework.data.repository.CrudRepository;
+import student.models.Student1;
+
+public interface StudentRepository1 extends CrudRepository<Student1,Long> {
+	
+
+
+}
